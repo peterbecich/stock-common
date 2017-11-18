396028579c210df750f353788fa9a9d7eef57074
Peter Becich <peterbecich@gmail.com>
2017-11-18 13:13:49 -0800

    fix port later

--- src/DB/Redis.hs
+++ src/DB/Redis.hs
@@ -16,15 +16,17 @@ getRedisConnection filePath = do
   db <- Config.subconfig "db" config
   redis <- Config.subconfig "redis" db
   ip <- Config.lookup "ip" redis
-  portStr <- Config.lookup "port" redis
+  -- portStr <- Config.lookup "port" redis
   let
     -- Socket.PortNumber distinct from
     -- Hedis' PortNumber!
-    port :: Sock.PortNumber
-    port = read portStr
+    -- TODO read port from config file
+    -- port :: Sock.PortNumber
+   -- port = read portStr
     connInfo = defaultConnectInfo
         { connectHost = ip
-        , connectPort = PortNumber port
+        -- , connectPort = PortNumber port
+        -- , connectPort = 6379
         }
   -- connect connInfo
   checkedConnect connInfo
