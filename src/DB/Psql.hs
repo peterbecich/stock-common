2150bc98755d184a10a0d66d406fce60ca339913
Peter Becich <peterbecich@gmail.com>
2017-11-27 19:42:13 -0800

    notes on runQuery

--- src/DB/Psql.hs
+++ src/DB/Psql.hs
@@ -2,8 +2,11 @@
 
 module DB.Psql where
 
+import Control.Monad (return)
+import Opaleye.RunQuery (runQuery)
 import Database.PostgreSQL.Simple
 import qualified Data.Yaml.Config as Config
+import qualified Opaleye.Internal.QueryArr as OE (Query)
 
 commonFilePath :: FilePath
 commonFilePath = "conf/common.yaml"
@@ -25,4 +28,24 @@ getPsqlConnection filePath = do
 
 closePsqlConnection :: Connection -> IO ()
 closePsqlConnection conn = close conn
+
+
+-- wastefully create and close Postgres connection for every call
+-- don't attempt to wrap runQuery like this;
+-- type parameters are not resolved
+-- https://www.stackage.org/haddock/lts-9.10/opaleye-0.5.4.0/Opaleye-RunQuery.html#v:runQuery
+-- getPsqlConnRun :: (Connection -> IO a) -> IO a
+-- getPsqlConnRun f = do
+--   psqlConn <- getPsqlConnection commonFilePath
+--   a <- f psqlConn
+--   closePsqlConnection psqlConn
+--   return a
+
+-- getPsqlConnRunQuery :: (OE.Query a) -> IO [a]
+-- getPsqlConnRunQuery query = do
+--   psqlConn <- getPsqlConnection commonFilePath
+--   x <- runQuery psqlConn query
+--   closePsqlConnection psqlConn
+--   return x
   
+
