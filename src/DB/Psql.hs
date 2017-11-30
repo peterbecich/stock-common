45f60082829ff53778bf07b0da7735eadd74c21f
Peter Becich <peterbecich@gmail.com>
2017-11-30 13:29:20 -0800

    Postgres connection pool

--- src/DB/Psql.hs
+++ src/DB/Psql.hs
@@ -1,17 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module DB.Psql where
 
 import Control.Monad (return)
+import Data.Profunctor.Product.Default
 import Opaleye.RunQuery (runQuery)
-import Database.PostgreSQL.Simple
+import Opaleye.Internal.RunQuery (QueryRunner)
+import Opaleye.Internal.QueryArr (Query)
+import qualified Data.Pool as Pool
 import qualified Data.Yaml.Config as Config
+import qualified Database.PostgreSQL.Simple as PSQL
 import qualified Opaleye.Internal.QueryArr as OE (Query)
 
 commonFilePath :: FilePath
 commonFilePath = "conf/common.yaml"
 
-getPsqlConnection :: FilePath -> IO Connection
+getConnInfo :: FilePath -> IO PSQL.ConnectInfo
+getConnInfo filePath = do
+  config <- Config.load filePath
+  db <- Config.subconfig "db" config
+  postgres <- Config.subconfig "postgres" db
+  dbname <- Config.lookup "dbname" postgres
+  user <- Config.lookup "user" postgres
+  password <- Config.lookup "password" postgres
+  ip <- Config.lookup "ip" postgres
+  port <- Config.lookup "port" postgres
+  let
+    connInfo :: PSQL.ConnectInfo
+    connInfo = PSQL.ConnectInfo ip port user password dbname
+  return connInfo
+
+getPsqlConnection :: FilePath -> IO PSQL.Connection
 getPsqlConnection filePath = do
   config <- Config.load filePath
   db <- Config.subconfig "db" config
@@ -22,30 +42,28 @@ getPsqlConnection filePath = do
   ip <- Config.lookup "ip" postgres
   port <- Config.lookup "port" postgres
   let
-    connInfo :: ConnectInfo
-    connInfo = ConnectInfo ip port user password dbname
-  connect connInfo
+    connInfo :: PSQL.ConnectInfo
+    connInfo = PSQL.ConnectInfo ip port user password dbname
+  PSQL.connect connInfo
 
-closePsqlConnection :: Connection -> IO ()
-closePsqlConnection conn = close conn
+closePsqlConnection :: PSQL.Connection -> IO ()
+closePsqlConnection conn = PSQL.close conn
 
-
--- wastefully create and close Postgres connection for every call
--- don't attempt to wrap runQuery like this;
--- type parameters are not resolved
+-- https://gist.github.com/thoughtpolice/e89d370f91039774f98bccf21e0cd877
+-- https://hackage.haskell.org/package/resource-pool-0.2.3.2/docs/Data-Pool.html
 -- https://www.stackage.org/haddock/lts-9.10/opaleye-0.5.4.0/Opaleye-RunQuery.html#v:runQuery
--- getPsqlConnRun :: (Connection -> IO a) -> IO a
--- getPsqlConnRun f = do
---   psqlConn <- getPsqlConnection commonFilePath
---   a <- f psqlConn
---   closePsqlConnection psqlConn
---   return a
 
--- getPsqlConnRunQuery :: (OE.Query a) -> IO [a]
--- getPsqlConnRunQuery query = do
---   psqlConn <- getPsqlConnection commonFilePath
---   x <- runQuery psqlConn query
---   closePsqlConnection psqlConn
---   return x
+type PostgresPool = Pool.Pool PSQL.Connection
+
+createPostgresPool :: FilePath
+                   -> IO PostgresPool
+createPostgresPool filePath = do
+  connInfo <- getConnInfo filePath
+  Pool.createPool (PSQL.connect connInfo) PSQL.close 4 1 4
   
-
+  
+runQueryPool :: Default QueryRunner cols vals
+             => Pool.Pool PSQL.Connection
+             -> Query cols
+             -> IO [vals]
+runQueryPool pool query = Pool.withResource pool (\conn -> runQuery conn query)
