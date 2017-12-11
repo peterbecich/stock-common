681f5d2ac94a707668a15e1095976a15ef38482e
Peter Becich <peterbecich@gmail.com>
2017-12-10 18:24:01 -0800

    tweak

--- scripts/merge_script.sh
+++ scripts/merge_script.sh
@@ -1,7 +1,7 @@
 #https://reformatcode.com/code/git/automatic-merge-branch-into-master-on-sucessful-build-in-travis
 
 echo "merge_script.sh"
-if [ "$TRAVIS_BRANCH" == "master"]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "merge into production"
 
     git checkout production || exit
@@ -9,6 +9,6 @@ if [ "$TRAVIS_BRANCH" == "master"]; then
     git push origin production || exit
 fi
 
-if [ "$TRAVIS_BRANCH" == "production"]; then
+if [ "$TRAVIS_BRANCH" == "production" ]; then
     echo "CI build of production branch successful"
 fi
