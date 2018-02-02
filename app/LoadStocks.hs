d900ac82ccd117f650692ef5a00a5680922db5cf
Peter Becich <peterbecich@gmail.com>
2018-02-01 22:51:14 -0800

    delay before loading stocks

--- app/LoadStocks.hs
+++ app/LoadStocks.hs
@@ -9,12 +9,18 @@ module Main where
 import DB.Psql
 import Types.Exchange.Psql
 import Util.LoadStocks
+
+import Control.Concurrent (threadDelay)
+
 -- import qualified Data.Pool as Pool
 
 main :: IO ()
 main = do
   putStrLn "load exchanges and stocks"
+  putStrLn "sleep for 10 seconds to allow Postgres tables to initialize"
+  threadDelay 10000000
 
+  putStrLn "load exchanges and stocks"
   psqlPool <- createPostgresPool commonFilePath
 
   exchangeCount <- runInsertPool psqlPool exchangeTable (exchangeToPsql <$> exchanges)
